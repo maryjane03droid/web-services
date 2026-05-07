e55719689fd5e6185e371a3fc0247135e44415ff
Maryjane <kuthiemaryjane@gmail.com>
2026-05-07 18:00:27 +0300

    import neccessary libraries

---
+++ pricescraper.py
@@ -0,0 +1,8 @@
+import requests  # For making HTTP requests to websites and APIs
+from bs4 import BeautifulSoup  # For parsing HTML content and extracting data
+import pandas as pd  # For creating nice tables and data manipulation
+import json  # For saving data in JSON format
+from datetime import datetime  # For timestamping when conversions happen
+import time  # For adding delays between requests (to be respectful to servers)
+import matplotlib.pyplot as plt  # For creating bar charts to visualize prices
+import re 
